49416561b468070bcd9af9cb083e30a93fe6a85d
Pino MV <pino@iessarenal.net>
2024-11-24 11:38:49 +0100

    Versió millorada (amb un while)

--- llenguatges.py
+++ llenguatges.py
@@ -1,4 +1,10 @@
 #Nova funcionalitat
 
-llenguatges = input("Introdueix els llenguatges:")
-print("Llenguatges:", llenguatges)
+print("Introdueix els llenguatges de programació que domines:")
+print("Per acabar introdueix un 0.")
+llenguatge = input()
+llenguatges_list = []
+while llenguatge != "0":
+    llenguatges_list.append(llenguatge)
+    llenguatge = input()
+print("Llenguatges:", llenguatges_list)
